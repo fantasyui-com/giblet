b8501e2f081746a206af15d71c421eb3850564fc
fantasyui-com <captain@fantasyui.com>
2019-08-06 10:09:11 -0400

    Initial UI for Lofi Hiphop Song Properties

---
+++ components/SongProperties.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <b-form>
+
+     <b-form-group id="input-group-1" label="Song name:" label-for="input-1" description="Specify song name." >
+       <b-form-input id="input-1" type="text" required placeholder="Song Name" ></b-form-input>
+     </b-form-group>
+
+  </b-form>
+
+</template>
