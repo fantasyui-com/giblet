eb57bf960e739107b5af736dd1938bfb2e88a96b
fantasyui-com <captain@fantasyui.com>
2019-08-07 10:23:39 -0400

    Initial Interface Layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,166 @@
-<template>
-  <div class="container">
-    <div>
-
-      <logo />
-      <h1 class="title">
-        giblet
-      </h1>
-      <h2 class="subtitle">
-        Lofi Hiphop
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Logo.vue';
+import SongProperties from '~/components/SongProperties.vue';
 
 export default {
   components: {
-    Logo
+    Logo,
+    SongProperties
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+
+
+.container, .row {
+  padding-top: 1rem;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
+
+<template>
+
+<b-container class="bv-example-row">
+  <b-row>
+    <b-col>
+
+
+      <div>
+
+        <b-card-group deck>
+
+
+        <b-card title="Song" header-tag="header" footer-tag="footer">
+          <h6 slot="header" class="mb-0">Song Properties</h6>
+          <b-card-text>Header and footers using slots.</b-card-text>
+
+
+          <song-properties />
+
+
+           <em slot="footer"><i class="text-muted">Describe your song properties</i></em>
+        </b-card>
+
+          <b-card title="Song Preview" header-tag="header" footer-tag="footer">
+            <h6 slot="header" class="mb-0">Song Preview</h6>
+            <b-card-text>Header and footers using slots.</b-card-text>
+
+
+            <b-button href="#" variant="primary">Preview Song</b-button>
+            <em slot="footer"><i class="text-muted">Footer Slotdd</i></em>
+          </b-card>
+
+        </b-card-group>
+
+      </div>
+
+    </b-col>
+  </b-row>
+
+
+
+
+
+
+
+
+
+  <b-row>
+    <b-col>
+
+
+      <div>
+
+        <b-card-group deck>
+
+
+        <b-card title="Samples" header-tag="header" footer-tag="footer">
+          <h6 slot="header" class="mb-0">Song Properties</h6>
+          <b-card-text>Header and footers using slots.</b-card-text>
+
+
+          <song-properties />
+
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+          <em slot="footer"><i class="text-muted">Describe your song properties</i></em>
+        </b-card>
+
+
+        </b-card-group>
+
+      </div>
+
+    </b-col>
+  </b-row>
+
+
+
+
+
+
+
+
+    <b-row>
+      <b-col>
+
+
+        <div>
+
+          <b-card-group deck>
+
+
+            <b-card no-body header="Card with flush list group">
+              <b-list-group flush>
+                <b-list-group-item href="#">Intro</b-list-group-item>
+                <b-list-group-item href="#">Solo</b-list-group-item>
+                <b-list-group-item href="#">Choir</b-list-group-item>
+                <b-list-group-item href="#">Drums</b-list-group-item>
+                <b-list-group-item href="#">Ending</b-list-group-item>
+                <b-list-group-item href="#">Outro</b-list-group-item>
+              </b-list-group>
+
+              <b-card-body>
+                Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor.
+              </b-card-body>
+            </b-card>
+
+
+          <b-card title="Structure" header-tag="header" footer-tag="footer">
+            <h6 slot="header" class="mb-0">Song Properties</h6>
+            <b-card-text>Header and footers using slots.</b-card-text>
+
+
+            <song-properties />
+
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+            <em slot="footer"><i class="text-muted">Describe your song properties</i></em>
+          </b-card>
+
+
+          </b-card-group>
+
+        </div>
+
+      </b-col>
+    </b-row>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</b-container>
+
+</template>
